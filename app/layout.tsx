4c6cf366039409e5506b565b14e30bd359293ba7
Timothy Lim <team@bitnovatelabs.com>
2026-02-24 12:16:55 +0800

    Disabled mobile pinch zoom and swipeable behaviour

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,9 @@ export const viewport: Viewport = {
   themeColor: "#2563eb",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
